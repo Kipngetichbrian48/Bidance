652d3865952f5e80c92ec568d9fd4b4dd8bf7f6c
Kipngetichbrian48 <brotich438@gmail.com>
2025-07-10 14:30:10 +0300

    Initial commit with Bootstrap setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5 custom-container">
+      <h1 className="text-primary">Welcome to Bidance Clone</h1>
+      <p className="lead">This is a Bootstrap-styled React app.</p>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
